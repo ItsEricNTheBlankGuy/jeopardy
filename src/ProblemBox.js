7d7a41c277f60610e6aef6ea66e4d68412a6f7c6
Eric <eric.nguyenblank@gmail.com>
2020-04-16 01:34:45 +0000

    your message

--- src/ProblemBox.js
+++ src/ProblemBox.js
@@ -1,13 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
 export default function ProblemBox(props){
-
-    function clues(){
-        alert(props.question);
+    let [clickCounter, setClickCounter] = useState(0);
+    let [message, setMessage] = useState(props.point);
+    function click(){
+        const newClickCounter = clickCounter + 1;
+        setClickCounter(newClickCounter);
+        if (newClickCounter === 1) {
+            setMessage(props.question);
+        } else if (newClickCounter === 2) {
+            setMessage(props.answer);
+            setClickCounter(-1);
+        } else if (newClickCounter === 0) {
+            setMessage(props.point);
+        }
+        alert();
     }
     return(
         <div>
-            <div onClick={clues}>
-                <h1>{props.point}</h1>
+            <div onClick={click}>
+                <h1>{message}</h1>
             </div>
             
         </div>
